5aa642a5b0f08d28b51fdc2ec43337a5c83b710c
Максим <mkaminskij994@gmail.com>
2026-04-11 11:07:03 +0300

    feat:new massives

--- src/data/traks.js
+++ src/data/traks.js
@@ -1,10 +1,52 @@
 const Megadeth = [
     {
-        id: 1,
-        title:"Hoy wars... The Punishment Due",
+        id: 1.1,
+        title:"Holy wars... The Punishment Due",
         artist:"Megadeth",
-        album:"Rust in piece",
-        cover:"public/img/rip.png",
-        track:"public/music/Megadeth - Holy Wars… The Punishment Due.mp3"
+        album:"Rust in peace",
+        cover:"..public/img/rip.png",
+        track:"../public/music/Megadeth - Holy Wars… The Punishment Due.mp3"
+    },
+    {
+        id: 1.2,
+        title:"Hangar18",
+        artist:"Megadeth",
+        album:"Rust in peace",
+        cover:"../public/img/rip.png",
+        track:"../public/music/Megadeth - Hangar 18.mp3"
+    },
+    {
+        id: 1.3,
+        title:"Tornado of Souls",
+        artist:"Megadeth",
+        album:"Rust in peace",
+        cover:"../public/img/rip.png",
+        track:"../public/music/Megadeth - Tornado Of Souls.mp3"
+    },
+    {
+        id: 1.4,
+        title:"Angry Again",
+        artist:"Megadeth",
+        album:"Hidden Treasures",
+        cover:"../public/img/HT.png",
+        track:"../public/music/Megadeth - Angry Again.mp3"
     }
 ]  
+const Pantera =[
+    {
+        id: 2.1,
+        title:"10s",
+        artist:"Pantera",
+        album:"The Great Southern Trendkill",
+        cover:"..public/img/TGST.png",
+        track:"../public/music/Pantera - 10s.mp3"
+    },
+    {
+        id: 2.2,
+        title:"Floods",
+        artist:"Pantera",
+        album:"The Great Southern Trendkill",
+        cover:"..public/img/TGST.png",
+        track:"../public/music/Pantera - Floods.mp3"
+    },
+]
