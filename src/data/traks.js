9cf0a7e64698ad43ee6b4e920a3d434a6325f3bc
Максим <mkaminskij994@gmail.com>
2026-04-11 10:22:56 +0300

    feat: js

---
+++ src/data/traks.js
@@ -0,0 +1,10 @@
+const Megadeth = [
+    {
+        id: 1,
+        title:"Hoy wars... The Punishment Due",
+        artist:"Megadeth",
+        album:"Rust in piece",
+        cover:"public/img/rip.png",
+        track:"public/music/Megadeth - Holy Wars… The Punishment Due.mp3"
+    }
+]  
